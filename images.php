b61bad29fe8cfb4f6e62671862672e4cf0dad78c
stardust-coder <stardust13551@gmail.com>
2020-08-11 07:39:25 +0900

    first commit

---
+++ images.php
@@ -0,0 +1,4 @@
+[ { "path": "img/img01.jpg", "name": "hogehoge", "caption": "サンプル１", "date": "8/11", "memo": "hoge" },
+ { "path": "img/img02.jpg", "name": "hoge", "caption": "サンプル２", "date": "8/11", "memo": "hogehgoe" },
+ { "path": "img/img03.jpg", "name": "hogehgoehoge", "caption": "サンプル３", "date": "8/11", "memo": "hogehogehoge" }
+  ]
